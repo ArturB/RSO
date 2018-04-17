fdace1d1c3d759a4e2ba899bc04582ae15fda53c
Artur M. Brodzki <artur.brodzki@gmail.com>
2018-04-17 21:03:59 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
-rm -rf RSO
-git clone https://github.com/ArturB/RSO
-cd RSO
+# check if repo dir exists
+if [ -d RSO ]; then
+# if so, update git repository
+  cd RSO && git pull
+else
+# otherwise, clone whole repo
+  git clone https://github.com/ArturB/RSO && cd RSO
+fi
+
+# deploy all docker-compose containers. 
+# sudo password is securely kept at the remote host. 
 cat ~/password | sudo -S docker stack deploy -c docker-stack.yml rso
 
