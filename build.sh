27e0210f08dd48f2546b1864ac86df761938594d
bartek <bczerwinski462@gmail.com>
2018-04-27 19:23:05 +0200

    Bugfixed  build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ mvn -f service1/pom.xml install
 mvn -f service2/pom.xml install
 
 # Optional additional tasks
-msapp/mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build
-mygateway/mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build
-myuaa/mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build
+( cd msapp ; mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build )
+( cd mygateway ; mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build )
+( cd myuaa ; mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build )
