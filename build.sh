532b87ba19439197b95c843c53539e654360efc9
bartek <bczerwinski462@gmail.com>
2018-04-29 01:14:57 +0200

    Removed new microservices building

--- build.sh
+++ build.sh
@@ -3,6 +3,3 @@ mvn -f service1/pom.xml install
 mvn -f service2/pom.xml install
 
 # Optional additional tasks
-( cd msapp ; bash mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build )
-( cd mygateway ; bash mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build )
-( cd myuaa ; bash mvnw package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" dockerfile:build )
