d8c32ae16344d9b727e9f0d519afe2b1d1706604
bartek <bczerwinski462@gmail.com>
2018-05-03 11:03:22 +0200

    fixed ssl generation 12

--- build.sh
+++ build.sh
@@ -6,9 +6,10 @@ mvn -f service2/pom.xml install
 ( cd msapp ; mvn package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" ; cd target ; )
 ls
 ls mygateway
-ls mygateway/target
-mv keystore.p12 mygateway/target/keystore.p12
+
 ( cd mygateway ; mvn package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" ; cd target ; )
+mkdir mygateway/target/ssl
+mv mygateway/ssl/keystore.p12 mygateway/target/ssl/keystore.p12
 
 ( cd myuaa ; mvn package -DskipTests -P"dev,no-liquibase,zuul,no-eureka" ; cd target ; )
 
