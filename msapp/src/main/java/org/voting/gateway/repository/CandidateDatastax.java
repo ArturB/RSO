6008ecd6238a61e2cdc3a397bd68766819b1cc32
Saragog <andrzejdackiewicz@gmail.com>
2018-05-28 22:30:27 +0200

    Further working on database connectivity

---
+++ msapp/src/main/java/org/voting/gateway/repository/CandidateDatastax.java
@@ -0,0 +1,48 @@
+package org.voting.gateway.repository;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.voting.gateway.domain.Candidate;
+
+public class CandidateDatastax {
+
+	Cluster cluster = null;
+	ResultSet rs = null;
+	Row row = null;
+	Session session = null;
+	
+	public CandidateDatastax()
+	{
+		try {
+		    cluster = Cluster.builder()                                                    	// (1)
+		            .addContactPoint("cass1.brodzki.org")
+		            .withCredentials("rso", "rso2018L")
+		            .build();
+		    session = cluster.connect("rso");                                       		// (2)
+
+		    rs = session.execute("select release_version from system.local");    			// (3)
+		    row = rs.one();
+		    System.out.println(row.getString("release_version"));                          	// (4)
+		} finally {
+		    //if (cluster != null) cluster.close();                                         // (5)
+		}
+	}
+
+	public List<Candidate> findAll()
+	{
+		System.out.println("Blablabla");
+		
+	    rs = session.execute("select release_version from system.local");    			// (3)
+	    row = rs.one();
+	    System.out.println(row.getString("release_version"));                          	// (4)
+		
+		return new LinkedList<Candidate>();
+	}
+	
+}
