ec4ba2f8a0ec06616d27f7aa01ca5f87a93fea75
Saragog <andrzejdackiewicz@gmail.com>
2018-06-02 00:49:52 +0200

    Further works on database classes

--- msapp/src/main/java/org/voting/gateway/domain/Turn.java
+++ msapp/src/main/java/org/voting/gateway/domain/Turn.java
@@ -1,5 +1,6 @@
 package org.voting.gateway.domain;
 
+import java.util.Date;
 import java.util.UUID;
 
 import com.datastax.driver.mapping.annotations.Column;
@@ -7,8 +8,6 @@ import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
 
 @Table(name = "candidate",keyspace = "rso",
-//readConsistency = "QUORUM",
-//writeConsistency = "QUORUM",
 caseSensitiveKeyspace = false,
 caseSensitiveTable = false)
 public class Turn {
@@ -19,8 +18,61 @@ public class Turn {
     @Column(name = "turn_id")
     private UUID id;
 
-    @Column(name = "age")
-    private Short age;
+    @Column(name = "commune")
+    private Short commune;
     
-    //@Column()
+    @Column(name = "date_from")
+    private Date dateFrom;
+    
+    @Column(name = "date_to")
+    private Date dateTo;
+    
+    @Column(name = "no_turn")
+    private Integer noTurn;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public Short getCommune() {
+		return commune;
+	}
+
+	public void setCommune(Short commune) {
+		this.commune = commune;
+	}
+
+	public Date getDateFrom() {
+		return dateFrom;
+	}
+
+	public void setDateFrom(Date dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	public Date getDateTo() {
+		return dateTo;
+	}
+
+	public void setDateTo(Date dateTo) {
+		this.dateTo = dateTo;
+	}
+
+	public Integer getNoTurn() {
+		return noTurn;
+	}
+
+	public void setNoTurn(Integer noTurn) {
+		this.noTurn = noTurn;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+    
+    
 }
--- msapp/src/main/java/org/voting/gateway/domain/VotingResults.java
+++ msapp/src/main/java/org/voting/gateway/domain/VotingResults.java
@@ -1,5 +1,148 @@
 package org.voting.gateway.domain;
 
-public class VotingResults {
+import java.util.Date;
+import java.util.UUID;
 
+import com.datastax.driver.mapping.annotations.Column;
+import com.datastax.driver.mapping.annotations.PartitionKey;
+import com.datastax.driver.mapping.annotations.Table;
+
+@Table(name = "candidate",keyspace = "rso",
+caseSensitiveKeyspace = false,
+caseSensitiveTable = false)
+public class VotingResults {
+	
+    @PartitionKey
+    @Column(name = "result_id")
+    private UUID id;
+
+    @Column(name = "candidate_id")
+    private Short candidateId;
+    
+    @Column(name = "candidate_name")
+    private String candidateName;
+    
+    @Column(name = "candidate_surname")
+    private String candidateSurname;
+    
+    @Column(name = "commune_name")
+    private String communeName;
+    
+    @Column(name = "date_generated")
+    private Date dateGenerated;
+    
+    @Column(name = "is_voting_finished")
+    private boolean isVotingFinished;
+    
+    @Column(name = "no_can_vote")
+    private int noCanVote;
+    
+    @Column(name = "no_cards_used")
+    private int noCardsUsed;
+    
+    @Column(name = "no_of_votes")
+    private int noOfVotes;
+    
+    @Column(name = "no_turn")
+    private int noTurn;
+    
+    @Column(name = "vote_type")
+    private String voteType;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public Short getCandidateId() {
+		return candidateId;
+	}
+
+	public void setCandidateId(Short candidateId) {
+		this.candidateId = candidateId;
+	}
+
+	public String getCandidateName() {
+		return candidateName;
+	}
+
+	public void setCandidateName(String candidateName) {
+		this.candidateName = candidateName;
+	}
+
+	public String getCandidateSurname() {
+		return candidateSurname;
+	}
+
+	public void setCandidateSurname(String candidateSurname) {
+		this.candidateSurname = candidateSurname;
+	}
+
+	public String getCommuneName() {
+		return communeName;
+	}
+
+	public void setCommuneName(String communeName) {
+		this.communeName = communeName;
+	}
+
+	public Date getDateGenerated() {
+		return dateGenerated;
+	}
+
+	public void setDateGenerated(Date dateGenerated) {
+		this.dateGenerated = dateGenerated;
+	}
+
+	public boolean isVotingFinished() {
+		return isVotingFinished;
+	}
+
+	public void setVotingFinished(boolean isVotingFinished) {
+		this.isVotingFinished = isVotingFinished;
+	}
+
+	public int getNoCanVote() {
+		return noCanVote;
+	}
+
+	public void setNoCanVote(int noCanVote) {
+		this.noCanVote = noCanVote;
+	}
+
+	public int getNoCardsUsed() {
+		return noCardsUsed;
+	}
+
+	public void setNoCardsUsed(int noCardsUsed) {
+		this.noCardsUsed = noCardsUsed;
+	}
+
+	public int getNoOfVotes() {
+		return noOfVotes;
+	}
+
+	public void setNoOfVotes(int noOfVotes) {
+		this.noOfVotes = noOfVotes;
+	}
+
+	public int getNoTurn() {
+		return noTurn;
+	}
+
+	public void setNoTurn(int noTurn) {
+		this.noTurn = noTurn;
+	}
+
+	public String getVoteType() {
+		return voteType;
+	}
+
+	public void setVoteType(String voteType) {
+		this.voteType = voteType;
+	}
+    
 }
