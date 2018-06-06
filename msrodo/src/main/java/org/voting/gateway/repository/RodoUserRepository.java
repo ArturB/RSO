1f95e2d8b6321d1e1443b6768973fbe32ecd0563
Saragog <andrzejdackiewicz@gmail.com>
2018-06-06 13:40:12 +0200

    Working on Rodo connectivity

--- msrodo/src/main/java/org/voting/gateway/repository/RodoUserRepository.java
+++ msrodo/src/main/java/org/voting/gateway/repository/RodoUserRepository.java
@@ -14,24 +14,22 @@ public class RodoUserRepository {
 	private Mapper<RodoUser> mapper;
 	private final CassandraSession cassandraSession;
 	
-	// TODO zmiany by laczylo sie z mikrousluga rodo
-	
     public RodoUserRepository(CassandraSession cassandraSession) {
-        //this.cassandraSession = cassandraSession;
-        //mapper = cassandraSession.getMappingManager().mapper(RodoUser.class);
+        this.cassandraSession = cassandraSession;
+        mapper = cassandraSession.getMappingManager().mapper(RodoUser.class);
     }
     
     public RodoUser findOne(UUID id) {
-    	//RodoUser res = mapper.get(id);
-    	//return res;
+    	RodoUser res = mapper.get(id);
+    	return res;
     }
     
     public RodoUser save(RodoUser user) {
-    	//mapper.save(user);
-    	//return user;
+    	mapper.save(user);
+    	return user;
     }
     
     public void delete(UUID id) {
-    	//mapper.delete(id);
+    	mapper.delete(id);
     }
 }
