4831efa79b734e31a18e27ae4df042ac36339564
bartek <bczerwinski462@gmail.com>
2018-06-07 21:20:58 +0200

    merged during rebase

--- mygateway/src/main/webapp/app/entities/votes-sum/votes-sum.controller.js
+++ mygateway/src/main/webapp/app/entities/votes-sum/votes-sum.controller.js
@@ -15,12 +15,14 @@
         var vm = this;
         vm.municipalities = Municipality.query();
         vm.electoralDistricts = [];
-        vm.selectedDistrictId = null;
+        vm.selectedDistrict = null;
+        vm.selectedMunicipality = null;
         vm.invalidVotes = 0;
-        vm.result = null;
+
 
         vm.selectedRound = 1;
         vm.rounds = [];
+        vm.result = null;
 
 
         ElectoralPeriod.getCurrentRound().then(function(round ){
@@ -53,33 +55,52 @@
 
         vm.idOfLastDistrict = null;
         vm.votesCache = null;
-        vm.getVotesFromDisctict = function(district) {
-            if(district){
-                if(vm.idOfLastDistrict === district.electoral_district_id){
-                    return vm.votesCache;
-                }else{
-                    vm.idOfLastDistrict = district.electoral_district_id;
+        vm.cachedSourceOfVotes = null;
+        vm.getVotes = function() {
+            var district = vm.selectedDistrict;
+            var municipality = vm.selectedMunicipality;
+            if (district && vm.cachedSourceOfVotes === "district" && vm.idOfLastDistrict === district.id) {
+                return vm.votesCache;
+            } else if (!district && municipality && vm.cachedSourceOfVotes === "municipality" && vm.idOfLastDistrict === municipality.id) {
+                return vm.votesCache;
+            } else if (district || municipality) {
+                var f = function () {
+                    // console.log("E41 TAKING!!");
                     vm.votesCache = [];
-                    VotesSum.votesSumFromDistrict(
-                    {
-                            districtId: district.electoral_district_id,
-                            round: vm.selectedRound
-                    }).$promise.then(function(result){
-                        vm.result = result;
-                        angular.forEach(result.candidate_votes, function(votePerCandidate){
-                            if(votePerCandidate.candidate_id !== -1){
-                                vm.votesCache.push({
-                                    candidate:Candidate.get({id:votePerCandidate.candidate_id}),
-                                    votesCount:votePerCandidate.number_of_votes
-                                });
-                            }
+                    if (district) {
+                        vm.idOfLastDistrict = district.id;
+                        vm.cachedSourceOfVotes = "district";
+                        return VotesSum.votesSumFromDistrict(
+                            {
+                                districtId: district.id,
+                                round: vm.selectedRound
+                            });
+                    } else if (municipality) {
+                        vm.cachedSourceOfVotes = "municipality";
+                        vm.idOfLastDistrict = municipality.id;
+                        return VotesSum.votesSumFromMunicipality(
+                            {
+                                municipalityId: municipality.id,
+                                round: vm.selectedRound
+                            });
+                    }
+                };
+
+                f().$promise.then(function (result) {
+                    vm.result = result;
+                    angular.forEach(result.candidate_votes, function (votePerCandidate) {
+                        vm.votesCache.push({
+                            candidate: Candidate.get({id: votePerCandidate.candidate_id}),
+                            votesCount: votePerCandidate.number_of_votes
                         });
                     });
-                    return vm.votesCache;
-                }
-            }else{
+                });
+                return vm.votesCache;
+            }
+            else {
                 vm.idOfLastDistrict = null;
                 vm.votesCache = null;
+                vm.cachedSourceOfVotes = null;
             }
         };
 
@@ -90,5 +111,16 @@
                 return "votesSumFinalRoundTable";
             }
         };
+
+        vm.votingIsFinished = function () {
+            if(vm.selectedDistrict) {
+                if (vm.selectedRound === 1) {
+                    return vm.selectedDistrict.first_round_votes_accepted;
+                } else {
+                    return vm.selectedDistrict.second_round_votes_accepted;
+                }
+            }
+        };
+
     }
 })();
